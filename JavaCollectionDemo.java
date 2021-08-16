e905345e724100a3ccf618c5253a1a2e07fe244a
KratiGupta <kratigupta8545@gmail.com>
2021-08-16 14:22:14 +0530

    Added welcome mesaage successfully

---
+++ JavaCollectionDemo.java
@@ -0,0 +1,12 @@
+package demolearningproblem;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class JavaCollectionDemo {
+	public static void main(String[] args) {
+		System.out.println("Welcome to master branch of Collections demo branch");
+		
+	}
+}
